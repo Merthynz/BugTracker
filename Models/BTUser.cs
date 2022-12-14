759a800368b87a0e5edda18872e0d4d75f83437a
Merthynz <rerive@gmail.com>
2022-12-13 20:59:27 -0500

    Add project files.

---
+++ Models/BTUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace BugTracker.Models
+{
+    public class BTUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [NotMapped]
+        [DisplayName("Full Name")]
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+    }
+}
