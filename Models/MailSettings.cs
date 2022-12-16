841b7e0383c288e3aa11c3171d8894fa0b6fd522
Merthynz <rerive@gmail.com>
2022-12-16 00:24:08 -0500

    Added Interface - IBTNotificationService, Implementation - BTNotificationService, Models - MailSettings, Updated Program

---
+++ Models/MailSettings.cs
@@ -0,0 +1,17 @@
+﻿#nullable disable
+
+namespace BugTracker.Models
+{
+    public class MailSettings
+    {
+        public string Mail { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+    }
+}
