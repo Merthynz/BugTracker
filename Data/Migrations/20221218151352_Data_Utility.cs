2ecc7e2e7e2429ad224b5f5e41b65783838a1663
Merthynz <rerive@gmail.com>
2022-12-18 11:08:11 -0500

    Added - class: DataUtility, enums: BTProjectPriority, BTTicketPriority, BTTicketStatus, BTTicketType, migrations: Data_Utility, Updated database

---
+++ Data/Migrations/20221218151352_Data_Utility.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BugTracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataUtility : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_AspNetUsers_OwnerUserid",
+                table: "Tickets");
+
+            migrationBuilder.RenameColumn(
+                name: "OwnerUserid",
+                table: "Tickets",
+                newName: "OwnerUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Tickets_OwnerUserid",
+                table: "Tickets",
+                newName: "IX_Tickets_OwnerUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_AspNetUsers_OwnerUserId",
+                table: "Tickets",
+                column: "OwnerUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_AspNetUsers_OwnerUserId",
+                table: "Tickets");
+
+            migrationBuilder.RenameColumn(
+                name: "OwnerUserId",
+                table: "Tickets",
+                newName: "OwnerUserid");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Tickets_OwnerUserId",
+                table: "Tickets",
+                newName: "IX_Tickets_OwnerUserid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_AspNetUsers_OwnerUserid",
+                table: "Tickets",
+                column: "OwnerUserid",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
