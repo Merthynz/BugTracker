a5bd92c52bc6bdb7ae88895ba161531b9cbcc10e
Merthynz <rerive@gmail.com>
2022-12-27 17:42:31 -0500

    Added AssignDeveloperViewModel ViewModels Model. Added AssignDeveloper Tickets View

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -13,6 +13,7 @@ using BugTracker.Models.Enums;
 using BugTracker.Services.Interfaces;
 using System.ComponentModel.Design;
 using Microsoft.AspNetCore.Authorization;
+using BugTracker.Models.ViewModels;
 
 #nullable disable
 
@@ -106,6 +107,29 @@ namespace BugTracker.Controllers
             }
         }
 
+        [HttpGet]
+        public async Task<IActionResult> AssignDeveloper(int id)
+        {
+            AssignDeveloperViewModel model = new();
+
+            model.Ticket = await _ticketService.GetTicketByIdAsync(id);
+            model.Developers = new SelectList(await _projectService.GetProjectMembersByRoleAsync(model.Ticket.ProjectId, nameof(Roles.Developer)), "Id", "FullName");
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AssignDeveloper(AssignDeveloperViewModel model)
+        {
+            if(model.DeveloperId != null)
+            {
+                await _ticketService.AssignTicketAsync(model.Ticket.Id, model.DeveloperId);
+                return RedirectToAction(nameof(Details), new { id = model.Ticket.Id });
+            }
+
+            return RedirectToAction(nameof(AssignDeveloper), new { id = model.Ticket.Id });
+        }
 
         // GET: Tickets/Details/5
         public async Task<IActionResult> Details(int? id)
